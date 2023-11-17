8998748ec538cb5543e4dcb81b76407eab087a64
Neto <tabalneto14@gmail.com>
2023-11-17 17:01:22 -0400

    Comentários adicionados

--- atv.js
+++ atv.js
@@ -1,95 +1,96 @@
 class Node {
+    // Definição da classe Node que representa um nó da árvore
     constructor(data, color) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-        this.color = color; // 'R' for red, 'B' for black
+        this.data = data;         // Valor do nó
+        this.left = null;         // Referência para o filho esquerdo
+        this.right = null;        // Referência para o filho direito
+        this.parent = null;       // Referência para o pai
+        this.color = color;       // Cor do nó ('R' para vermelho, 'B' para preto)
     }
 }
 
 class RedBlackTree {
+    // Definição da classe RedBlackTree que representa uma árvore Rubro-Negra
     constructor() {
-        this.root = null;
-        this.NIL = new Node(null, 'B');
+        this.root = null;         // Raiz da árvore
+        this.NIL = new Node(null, 'B');  // Nó nulo, utilizado como folha e com cor preta
         this.NIL.left = null;
         this.NIL.right = null;
         this.NIL.parent = null;
     }
 
     insert(data) {
-        let node = new Node(data, 'R');
+        // Inserção de um novo valor na árvore
+        let node = new Node(data, 'R');  // Novo nó a ser inserido é inicialmente vermelho
         if (!this.root) {
-            this.root = node;
-            this.root.color = 'B';  // Define a cor da raiz como preta
+            this.root = node;           // Se a árvore estiver vazia, o novo nó torna-se a raiz
+            this.root.color = 'B';      // A raiz é sempre preta
         } else {
-            this._insert(this.root, node);
-            this.fixInsert(node);
+            this._insert(this.root, node);  // Chamada à função de inserção recursiva
+            this.fixInsert(node);           // Ajusta a árvore após a inserção
         }
     }
 
     _insert(root, node) {
+        // Função auxiliar para a inserção recursiva de um nó na árvore
         if (node.data < root.data) {
+            // Se o valor a ser inserido é menor que o valor do nó atual, vai para a subárvore esquerda
             if (root.left === null) {
-                root.left = node;
+                root.left = node;      // Se não há filho esquerdo, insere o novo nó aqui
                 node.parent = root;
             } else {
-                this._insert(root.left, node);
+                this._insert(root.left, node);  // Caso contrário, continua a busca na subárvore esquerda
             }
         } else if (node.data > root.data) {
+            // Se o valor a ser inserido é maior que o valor do nó atual, vai para a subárvore direita
             if (root.right === null) {
-                root.right = node;
+                root.right = node;     // Se não há filho direito, insere o novo nó aqui
                 node.parent = root;
             } else {
-                this._insert(root.right, node);
+                this._insert(root.right, node); // Caso contrário, continua a busca na subárvore direita
             }
         }
     }
 
     fixInsert(node) {
+        // Ajusta a árvore após a inserção de um nó vermelho
         while (node.parent && node.parent.color === 'R') {
             if (node.parent === node.parent.parent.left) {
-                let uncle = node.parent.parent.right;
+                // O pai do nó é o filho esquerdo de seu avô
+                let uncle = node.parent.parent.right;  // Tio é o irmão do pai do nó
                 if (uncle && uncle.color === 'R') {
-                    console.log(`Troca de cor executada em ${node.parent.parent.data}`);
-                    if (node.parent.parent === this.root) {
-                        console.log(`Raiz detectada, então permanece preto`);
-                    } else {
-                        console.log(`Trocou a cor de black para red`);
-                    }
+                    // Caso 1: O tio é vermelho
                     node.parent.color = 'B';
                     uncle.color = 'B';
                     node.parent.parent.color = 'R';
                     node = node.parent.parent;
                 } else {
+                    // Caso 2: O tio é preto
                     if (node === node.parent.right) {
                         node = node.parent;
                         this.leftRotate(node);
                     }
                     node.parent.color = 'B';
                     node.parent.parent.color = 'R';
-                    console.log(`Troca de cor executada em ${node.parent.parent.data}`);
-                    console.log(`Trocou a cor de red para black`);
                     this.rightRotate(node.parent.parent);
                 }
             } else {
-                let uncle = node.parent.parent.left;
+                // O pai do nó é o filho direito de seu avô
+                let uncle = node.parent.parent.left;  // Tio é o irmão do pai do nó
                 if (uncle && uncle.color === 'R') {
+                    // Caso 1: O tio é vermelho
                     node.parent.color = 'B';
                     uncle.color = 'B';
                     node.parent.parent.color = 'R';
-                    console.log(`Troca de cor executada em ${node.parent.parent.data}`);
-                    console.log(`Trocou a cor de red para black`);
                     node = node.parent.parent;
                 } else {
+                    // Caso 2: O tio é preto
                     if (node === node.parent.left) {
                         node = node.parent;
                         this.rightRotate(node);
                     }
                     node.parent.color = 'B';
                     node.parent.parent.color = 'R';
-                    console.log(`Troca de cor executada em ${node.parent.parent.data}`);
-                    console.log(`Trocou a cor de red para black`);
                     this.leftRotate(node.parent.parent);
                 }
             }
@@ -98,6 +99,7 @@ class RedBlackTree {
     }
 
     leftRotate(x) {
+        // Rotação à esquerda
         let y = x.right;
         x.right = y.left;
         if (y.left !== this.NIL) {
@@ -116,6 +118,7 @@ class RedBlackTree {
     }
 
     rightRotate(y) {
+        // Rotação à direita
         let x = y.left;
         y.left = x.right;
         if (x.right !== this.NIL) {
@@ -134,6 +137,7 @@ class RedBlackTree {
     }
 
     remove(data) {
+        // Remove um nó com o valor fornecido da árvore
         let node = this.search(data);
         if (node) {
             console.log(`Realizando a remoção do nó com valor ${data}`);
@@ -142,6 +146,7 @@ class RedBlackTree {
     }
 
     _remove(node) {
+        // Função auxiliar para remoção de um nó da árvore
         let y = node;
         let yOriginalColor = y ? y.color : 'B';
         let x;
@@ -183,6 +188,7 @@ class RedBlackTree {
     }
 
     transplant(u, v) {
+        // Substitui a subárvore enraizada em u pela subárvore enraizada em v
         if (u.parent === null) {
             this.root = v;
         } else if (u === u.parent.left) {
@@ -197,6 +203,7 @@ class RedBlackTree {
     }
 
     minimum(node) {
+        // Encontra o nó com valor mínimo a partir de um determinado nó
         while (node && node.left !== this.NIL) {
             node = node.left;
         }
@@ -204,10 +211,12 @@ class RedBlackTree {
     }
 
     search(data) {
+        // Busca um valor na árvore
         return this._search(this.root, data);
     }
 
     _search(node, data) {
+        // Função auxiliar para a busca recursiva de um valor na árvore
         if (node === null || data === node.data) {
             return node;
         }
@@ -220,36 +229,29 @@ class RedBlackTree {
     }
 
     fixRemove(x) {
+        // Ajusta a árvore após a remoção de um nó preto
         while (x !== this.root && x.color === 'B') {
             if (x === x.parent.left) {
                 let w = x.parent.right;
                 if (w.color === 'R') {
-                    console.log(`Troca de cor executada em ${w.data}`);
-                    if (w === this.root) {
-                        console.log(`Raiz detectada, então permanece preto`);
-                    } else {
-                        console.log(`Manteve a cor vermelha (devido à rotação)`);
-                    }
+                    // Caso 1: O irmão (w) é vermelho
                     w.color = 'B';
                     x.parent.color = 'R';
                     this.leftRotate(x.parent);
                     w = x.parent.right;
                 } else if (w.right.color === 'B' && w.left.color === 'B') {
-                    console.log(`Troca de cor executada em ${w.data}`);
-                    console.log(`Manteve a cor preta`);
+                    // Caso 2: O irmão (w) é preto e ambos os filhos são pretos
                     w.color = 'R';
                     x = x.parent;
                 } else {
                     if (w.right.color === 'B') {
-                        console.log(`Troca de cor executada em ${w.left.data}`);
-                        console.log(`Trocou de red para black`);
+                        // Caso 3: O irmão (w) é preto e o filho direito é preto
                         w.left.color = 'B';
                         w.color = 'R';
                         this.rightRotate(w);
                         w = x.parent.right;
                     }
-                    console.log(`Troca de cor executada em ${w.data}`);
-                    console.log(`Trocou de red para ${x.parent.color}`);
+                    // Caso 4: O irmão (w) é preto e o filho direito é vermelho
                     w.color = x.parent.color;
                     x.parent.color = 'B';
                     w.right.color = 'B';
@@ -259,29 +261,25 @@ class RedBlackTree {
             } else {
                 let w = x.parent.left;
                 if (w.color === 'R') {
-                    console.log(`Troca de cor executada em ${w.data}`);
-                    console.log(`Trocou de red para black`);
+                    // Caso 1: O irmão (w) é vermelho
                     w.color = 'B';
                     x.parent.color = 'R';
                     this.rightRotate(x.parent);
                     w = x.parent.left;
                 }
                 if (w.right.color === 'B' && w.left.color === 'B') {
-                    console.log(`Troca de cor executada em ${w.data}`);
-                    console.log(`Trocou de red para black`);
+                    // Caso 2: O irmão (w) é preto e ambos os filhos são pretos
                     w.color = 'R';
                     x = x.parent;
                 } else {
                     if (w.left.color === 'B') {
-                        console.log(`Troca de cor executada em ${w.right.data}`);
-                        console.log(`Trocou de red para black`);
+                        // Caso 3: O irmão (w) é preto e o filho esquerdo é preto
                         w.right.color = 'B';
                         w.color = 'R';
                         this.leftRotate(w);
                         w = x.parent.left;
                     }
-                    console.log(`Troca de cor executada em ${w.data}`);
-                    console.log(`Trocou de red para ${x.parent.color}`);
+                    // Caso 4: O irmão (w) é preto e o filho esquerdo é vermelho
                     w.color = x.parent.color;
                     x.parent.color = 'B';
                     w.left.color = 'B';
@@ -289,16 +287,17 @@ class RedBlackTree {
                     x = this.root;
                 }
             }
-
         }
         x.color = 'B';
     }
 
     getRoot() {
+        // Retorna a raiz da árvore
         return this.root;
     }
 
     printTree(node = this.root, level = 0, prefix = 'ROOT') {
+        // Imprime a árvore de forma visual
         if (node !== null) {
             console.log('  '.repeat(level), `${prefix}(${node.color}) ${node.data}`);
             this.printTree(node.left, level + 1, 'Left');
@@ -307,6 +306,7 @@ class RedBlackTree {
     }
 
     removeRedNodes() {
+        // Remove automaticamente todos os nós vermelhos da árvore
         console.log("\nRemovendo nós vermelhos automaticamente:");
 
         const redNodes = this.findRedNodes(this.root);
@@ -319,6 +319,7 @@ class RedBlackTree {
     }
 
     findRedNodes(node, redNodes = []) {
+        // Encontra todos os nós vermelhos na árvore
         if (node !== null && node !== this.NIL) {
             if (node.color === 'R') {
                 redNodes.push(node);
@@ -330,27 +331,30 @@ class RedBlackTree {
     }
 
     removeRoot() {
+        // Remove a raiz da árvore
         if (this.root) {
             console.log(`Removendo a raiz com valor ${this.root.data}`);
             this._remove(this.root);
 
-            // Set a new root (successor of the removed root)
+            // Define uma nova raiz (sucessor da raiz removida)
             if (this.root !== null) {
                 while (this.root.parent !== null) {
                     this.root = this.root.parent;
                 }
-                this.root.color = 'B'; // Ensure the new root is black
+                this.root.color = 'B'; // Garante que a nova raiz seja preta
             }
         } else {
             console.log("A árvore está vazia. Não há raiz para remover.");
         }
     }
+    
     clearTree(node) {
+        // Limpa a árvore inteira
         if (node !== null && node !== this.NIL) {
             this.clearTree(node.left);
             this.clearTree(node.right);
 
-            // Check if 'node' is not null before accessing its properties
+            // Verifica se 'node' não é nulo antes de acessar suas propriedades
             if (node.parent !== null) {
                 if (node === node.parent.left) {
                     node.parent.left = null;
@@ -358,13 +362,12 @@ class RedBlackTree {
                     node.parent.right = null;
                 }
             } else {
-                // 'node' is the root
+                // 'node' é a raiz
                 this.root = null;
             }
         }
     }
 }
-
 
 // Teste
 
